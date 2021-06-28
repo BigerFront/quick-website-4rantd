3dbc88f48b61c909c4a2e7a2167eefc046d29b2f
developer <developer-team@gmail.com>
2021-06-28 23:50:48 +0800

    upd: 404 style

--- src/views/errors/404-page.jsx
+++ src/views/errors/404-page.jsx
@@ -14,7 +14,7 @@ export default function PageNotFound(props) {
         </div>
       </div>
 
-      <div className="error-page-info">
+      <div className="error-page__info">
         <h2>We can't find that page</h2>
         <p>
           We're fairly sure that page used to be here, but seems to have gone
